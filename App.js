4272d039eed30505f0d5d017df12dbff55b50a7d
andremugabo <andremugabo@yahoo.fr>
2023-07-15 00:00:58 +0200

    second commit

--- App.js
+++ App.js
@@ -1,20 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList, ActivityIndicator } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import NetInfo from "@react-native-community/NetInfo";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const API_URL = "https://api.example.com/posts";
+const STORAGE_KEY = "@OfflineApp:posts";
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [offlineData, setOfflineData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(API_URL);
+      const { data } = response;
+      setData(data);
+      setOfflineData(data);
+      AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const syncData = async () => {
+    try {
+      const offlineDataStr = await AsyncStorage.getItem(STORAGE_KEY);
+      const offlineData = JSON.parse(offlineDataStr) || [];
+      setOfflineData(offlineData);
+      const response = await axios.post(API_URL, offlineData);
+      const { data } = response;
+      setData(data);
+      AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const renderItem = ({ item }) => (
+    <View
+      style={{ padding: 16, borderBottomWidth: 1, borderBottomColor: "#ccc" }}
+    >
+      <Text>{item.title}</Text>
+      <Text>{item.body}</Text>
     </View>
   );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <View style={{ flex: 1 }}>
+      {loading ? (
+        <ActivityIndicator style={{ flex: 1 }} size="large" />
+      ) : (
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id.toString()}
+          ListEmptyComponent={() => (
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text>
+                No data available. Please check your internet connection.
+              </Text>
+            </View>
+          )}
+        />
+      )}
+    </View>
+  );
+};
+
+export default App;
+
+const handleConnectivityChange = (isConnected) => {
+  if (isConnected) {
+    syncData();
+  }
+};
+
+useEffect(() => {
+  const unsubscribe = NetInfo.addEventListener(handleConnectivityChange);
+
+  return () => unsubscribe();
+}, []);
